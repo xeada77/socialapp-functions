ed760a934957cb00db568edf542dd378ab66a724
Luis Martinez <luismartq@gmail.com>
2019-09-17 12:45:03 +0200

    Cambios inicializacion app

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./../../socialapp-cd66b-firebase-adminsdk-rgpp9-7d83259b74.json');
+//const serviceAccount = require('./../../socialapp-cd66b-firebase-adminsdk-rgpp9-7d83259b74.json');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https://socialapp-cd66b.firebaseio.com"
 });
 //admin.initializeApp();
 const db = admin.firestore();
